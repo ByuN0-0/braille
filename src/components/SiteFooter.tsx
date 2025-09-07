a53e4e846eb06842cfa9bcf6443bb1893d144b2e
byun0-0 <devbyeon@gmail.com>
2025-09-07 13:51:57 +0900

    chore: SiteFooter 컴포넌트에서 작성자 및 문의 정보를 통합하여 이메일 링크 수정

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -3,8 +3,7 @@ export default function SiteFooter() {
     <footer className="w-full border-t">
       <div className="container py-6 text-sm text-gray-600 flex flex-wrap items-center gap-x-3 gap-y-1">
         <span>© {new Date().getFullYear()} Biyeon Hwang. All rights reserved.</span>
-        <span className="whitespace-nowrap">만든이: <a href="mailto:devbyeon@gmail.com">devbyeon@gmail.com</a></span>
-        <span className="whitespace-nowrap">문의: <a href="mailto:devbyeon@gmail.com">devbyeon@gmail.com</a></span>
+        <span className="whitespace-nowrap">만든이 및 문의의: <a href="mailto:devbyeon@gmail.com">devbyeon@gmail.com</a></span>
       </div>
     </footer>
   );
